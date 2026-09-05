36a1ce03714ad5bc1793f37953999b18d05a0485
leonardo <leonardo.balk01@gmail.com>
2026-09-05 17:00:17 -0300

    refactor: remove home-teste experimental page
    The alternate home page concept at /home-teste served its comparison
    purpose and is not needed going forward. Removes the route, the page
    files, and its now-unused t.homeTest translation entries.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-import HomeTest from './pages/HomeTest.jsx'
 import { Route, Routes } from 'react-router-dom'
 import LessonScreen from './features/trilha-teste/LessonScreen.jsx'
 import SystemsPanel from './features/trilha-teste/SystemsPanel.jsx'
@@ -7,7 +6,6 @@ import Home from './pages/Home.jsx'
 function App() {
   return (
     <Routes>
-      <Route path="/home-teste" element={<HomeTest />} />
       <Route path="/" element={<Home />} />
       <Route path="/trilha" element={<SystemsPanel />} />
       <Route path="/trilha/:unitId" element={<LessonScreen />} />
